e525b65de79ae10586875a8475d64a49176c898b
Khaled Elaraby <khaled.elaraby@gp2it.com>
2024-10-28 19:25:31 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'To Do List',
+    'author':'Khaled GP',
+    'version': '1.0',
+    'category': '',
+    'depends': ['base'],
+    'data': [
+        'views/base_menu.xml',
+        'views/todo_list_views.xml',
+
+    ],
+    'application': True,
+    'installable': True,
+}
---
+++ models/todo_list.py
@@ -0,0 +1,15 @@
+from odoo import models, fields
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    _description = 'Task for Todo List'
+
+    name = fields.Char(string='Task Name', required=True)
+    assigned_to = fields.Many2one('res.users', string='Assigned To', required=True)
+    description = fields.Text(string='Description')
+    due_date = fields.Date(string='Due Date')
+    status = fields.Selection([
+        ('new', 'New'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    ], string='Status', default='new')
